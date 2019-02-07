114154d4ef27c157af13442efcd0f1cd828be21e
neilprado <neiljohn17@gmail.com>
2019-02-07 11:00:25 -0300

    Setting structure

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-  <p>Olar</p>    
+  <h1 class="mt-2">Lista de Posts</h1>
+  <a href="{{url('posts/create')}}" class="btn btn-primary mt-2 mb-3">Criar Post</a>   
+  @if (count($posts) > 0)
+    @foreach ($posts as $p)
+        <div class="card mt-3">
+          <div class="card-body">
+            <a href="{{url("/posts/$p->id")}}">
+              <p class="card-text">{{$t->name}}</p>
+            </a>
+          </div>
+        </div>
+    @endforeach
+  @else
+    <p class="card-text">Não há posts cadastrados</p>
+  @endif
 @endsection
--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <h1 class="mt-2">Lista de Temas</h1>
-    <a href="{{url("/temas/create")}}" class="btn btn-primary mt-2 mb-3">Criar Tema</a>
+  <a href="{{url("/temas/create")}}" class="btn btn-primary mt-2 mb-3">Criar Tema</a>
   @if (count($themes) > 0)
     @foreach ($themes as $t)
       <div class="card mt-3">
