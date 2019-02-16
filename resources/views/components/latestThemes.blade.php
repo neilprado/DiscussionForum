600364127c563303b00f4b7a95322c8b3665c06e
neilprado <neiljohn17@gmail.com>
2019-02-16 16:24:27 -0300

    Filtering themes with error

--- resources/views/components/latestThemes.blade.php
+++ resources/views/components/latestThemes.blade.php
@@ -1,3 +1,13 @@
 <h2>Últimos temas</h2>
-  
+@forelse ($themes as $t)
+<div class="card">
+  <div class="card-body">
+    <p>{{$t->name}}</p>
+  </div>
+</div>
+@empty
+<div class="card-body">
+  <p>Não há temas cadastrados</p>
+</div>  
+@endforelse
   
